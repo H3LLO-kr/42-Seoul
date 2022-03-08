58718ea0a81c62b55bb31c1126eb47bb73dc0a01
chanhapa <qkrcksgk02@korea.ac.kr>
2022-03-08 14:30:45 +0900

    Libft #0

---
+++ libft/ft_atoi.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chanhapa <chanhapa@student.42seoul.>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/08 14:11:09 by chanhapa          #+#    #+#             */
+/*   Updated: 2022/03/08 14:30:23 by chanhapa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	_is_space(char c)
+{
+	if ((c >= 9 && c <= 13) || c == ' ')
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(char *str)
+{
+	int	num;
+	int	sign;
+
+	num = 0;
+	sign = -1;
+	while (*str && _is_space(*str))
+		str++;
+	while (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		num *= 10;
+		num += *str - '0';
+		str++;
+	}
+	return (num * sign);
+}
