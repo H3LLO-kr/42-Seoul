dac2da14eb21d25cfe17087219398b8f986f29f7
Chanha Park <qkrcksgk02@korea.ac.kr>
2022-03-22 19:45:58 +0900

    GNL

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chanhapa <chanhapa@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/22 19:20:53 by chanhapa          #+#    #+#             */
+/*   Updated: 2022/03/22 19:33:18 by chanhapa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <limits.h>
+
+#endif
