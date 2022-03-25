4e52adf1739421c4a2e46d9fec6efb3d818c0867
박찬하 <parkchanha@bagchanhaui-MacBookAir.local>
2022-03-25 21:21:12 +0900

    Libft

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,25 +6,25 @@
 /*   By: chanhapa <chanhapa@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 22:55:53 by chanhapa          #+#    #+#             */
-/*   Updated: 2022/03/21 13:44:14 by chanhapa         ###   ########.fr       */
+/*   Updated: 2022/03/25 21:21:01 by chanhapa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(char *src)
+char	*ft_strdup(const char *s1)
 {
 	int		len;
 	char	*new_str;
 
 	len = 0;
-	while (src[len])
+	while (s1[len])
 		len++;
 	new_str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!new_str)
 		return (NULL);
 	new_str[len] = 0;
 	while (--len >= 0)
-		new_str[len] = src[len];
+		new_str[len] = s1[len];
 	return (new_str);
 }
